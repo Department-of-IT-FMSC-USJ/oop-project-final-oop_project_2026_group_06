0f398157b4b86bbc20744756e9ad72062bdf3020
yasasmi-subhagya <yasasmisubhagya2020@gmail.com>
2026-03-22 12:53:35 +0530

    Initial commit: add web project

---
+++ BusinessRegistrationSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BusinessRegistrationSystem.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
