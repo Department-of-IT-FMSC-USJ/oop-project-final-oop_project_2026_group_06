93b1bc6f4939434456e86b1ab6aa4b483181aa51
yasasmi-subhagya <yasasmisubhagya2020@gmail.com>
2026-05-17 21:39:03 +0530

    add admin related migrations

---
+++ BusinessRegistrationSystem/Migrations/20260302171414_SeedAdmin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BusinessRegistrationSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedAdmin : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "PasswordHash", "Role", "Username" },
+                values: new object[] { new Guid("00000000-0000-0000-0000-000000000001"), "4PrvXIsA6S8uQAsR4YgIpkpBy2y3w6u0FIn959d8pY4=", 0, "admin" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000001"));
+        }
+    }
+}
---
+++ BusinessRegistrationSystem/Migrations/20260310065625_InitialPostgresCreate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BusinessRegistrationSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialPostgresCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000001"),
+                column: "Username",
+                value: "sysadmin");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000001"),
+                column: "Username",
+                value: "admin");
+        }
+    }
+}
